d6a942a880f43addad1c0829261a29ef7f70e9dd
jcoenep <jcoenep@hotmail.com>
2016-04-30 21:34:24 +0800

    rodham img plot

--- rodham.R
+++ rodham.R
@@ -1,10 +1,6 @@
 
 library(rodham)
 library(igraph)
-library(ggnetwork)
-library(svgPanZoom)
-library(ggrepel)
-library(SVGAnnotation)
 
 data("emails")
 
@@ -14,21 +10,6 @@ nodes <- nodes_emails(emails)
 g <- graph.data.frame(edges, vertices = nodes)
 
 V(g)$color <- rev(colorRampPalette(RColorBrewer:::brewer.pal(5, "YlOrRd"))(length(V(g)$weight)))
-
-dat <- ggnetwork(g, layout="fruchtermanreingold", arrow.gap=0, cell.jitter=0)
-
-ggplot() +
-  geom_edges(data=dat, 
-             aes(x=x, y=y, xend=xend, yend=yend),
-             color="grey50", curvature=0.1, size=0.15, alpha=1/2) +
-  geom_nodes(data=dat,
-             aes(x=x, y=y, xend=xend, yend=yend, size=sqrt(weight.x)),
-             alpha=1/3) +
-  geom_label_repel(data=unique(dat[dat$size>50,c(1,2,5)]),
-                   aes(x=x, y=y, label=vertex.names), 
-                   size=2, color="#8856a7") +
-  theme_blank() +
-  theme(legend.position="none") -> gg
 
 plot(g, layout = layout.fruchterman.reingold(g), vertex.color = V(g)$color,
      vertex.label.family = "sans",
